f339260d5fa8a90d9a6fb6c42062f9543c5d05ed
Regina.Gimazova <reginagimazova98@gmail.com>
2018-12-08 01:21:46 +0300

    Updates

---
+++ precache-manifest.224c6736ba1fc4f32c302152049bfef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0373d64935abbc14e6e2",
+    "url": "/FS_project/static/js/runtime~main.0373d649.js"
+  },
+  {
+    "revision": "6e3b74f8ccafa9a22ee4",
+    "url": "/FS_project/static/js/main.6e3b74f8.chunk.js"
+  },
+  {
+    "revision": "64494fa28c699c52f649",
+    "url": "/FS_project/static/js/1.64494fa2.chunk.js"
+  },
+  {
+    "revision": "6e3b74f8ccafa9a22ee4",
+    "url": "/FS_project/static/css/main.c9251ae5.chunk.css"
+  },
+  {
+    "revision": "1dfd960bf8f2f40ed0d71b00ca4da9f7",
+    "url": "/FS_project/index.html"
+  }
+];
