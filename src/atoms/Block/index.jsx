bfda52e01f7c2b396ab073cd21923cbba047d795
Regina.Gimazova <reginagimazova98@gmail.com>
2018-12-07 22:35:09 +0300

    Updates

--- src/atoms/Block/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Block.module.css';
-
-const Block = () => (
-  <div className={styles.block} />
-);
-
-export default Block;
--- src/atoms/Label/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Label.module.css';
-
-const Label = props => (
-  <label className={styles.label} {...props}>{props.children}</label>
-);
-
-export default Label;
